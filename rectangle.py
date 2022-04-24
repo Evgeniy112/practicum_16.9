cfe600462f7f929fdc7e14ce9dc3217a3cb0f720
Evgeniy112 <evgeniy.pogorelov@bk.ru>
2022-04-24 12:11:56 +0300

    new

--- rectangle.py
+++ rectangle.py
@@ -4,3 +4,11 @@ class Rectangle:
         self.y = y
         self.width = width
         self.height = height
+    def __str__(self):
+        return f'Rectangle: {self.x}, {self.y}, {self.width}, {self.height}.'
+    def getArea(self):
+        return self.width * self.height
+
+rect_1 = Rectangle(5,10,50,100)
+print(rect_1)
+print(rect_1.getArea())
